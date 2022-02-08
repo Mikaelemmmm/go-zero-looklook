515d4e5a26bc7a8a9b45d6468997f89361ed0ea3
977231903@qq.com <>
2022-02-08 21:17:01 +0800

    fix goctl gen code

--- deploy/script/gencode/gen.sh
+++ deploy/script/gencode/gen.sh
@@ -1,13 +1,25 @@
-# 生成api ， 进入"服务/cmd"目录下，执行下面命令
-# goctl api go -api ./api/desc/*.api -dir ./api -style=goZero
+# 生成api ， 进入"服务/cmd/api/desc"目录下，执行下面命令
+# goctl api go -api *.api -dir ../  -style=goZero
 
-# 生成rpc ， 进入"服务/cmd"目录下，执行下面命令
-# goctl rpc proto -src rpc/pb/*.proto -dir ./rpc -style=goZero
-# 去除proto中的json的omitempty
-# sed -i 's/,omitempty//g'  ./rpc/pb/*.pb.go
+# 生成rpc
+# 【注】 需要安装下面3个插件
+#       protoc >= 3.13.0 ， 如果没安装请先安装 https://github.com/protocolbuffers/protobuf，下载解压即可
+#       protoc-gen-go ，如果没有安装请先安装 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+#       protoc-gen-go-grpc  ，如果没有安装请先安装 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+#
+#       如果有要使用grpc-gateway，也请安装如下两个插件 , 没有使用就忽略下面2个插件
+#       go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+#       go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
+#
+# 1）goctl >= 1.3 进入"服务/cmd/rpc/pb"目录下，执行下面命令
+#    goctl rpc protoc *.proto --go_out=../ --go-grpc_out=../  --zrpc_out=../
+#    去除proto中的json的omitempty
+#    sed -i "" 's/,omitempty//g' *.pb.go
+# 2）goctl < 1.3 进入"服务/cmd"目录下，执行下面命令
+#    goctl rpc proto -src rpc/pb/*.proto -dir ./rpc -style=goZero
+#    去除proto中的json的omitempty
+#    sed -i "" 's/,omitempty//g'  ./rpc/pb/*.pb.go
 
-# docker + air hot deployment basic
-# https://hub.docker.com/r/cosmtrek/air
 
 
 # 创建kafka的topic
