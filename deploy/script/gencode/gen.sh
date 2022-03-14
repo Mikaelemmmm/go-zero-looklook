28d0de5ab48acb042aab0f9969f7c7fcaf747df4
mikael <13247629622@163.com>
2022-03-14 12:22:00 +0800

    update gen rpc style=goZero

--- deploy/script/gencode/gen.sh
+++ deploy/script/gencode/gen.sh
@@ -12,7 +12,7 @@
 #       go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 #
 # 1）goctl >= 1.3 进入"服务/cmd/rpc/pb"目录下，执行下面命令
-#    goctl rpc protoc *.proto --go_out=../ --go-grpc_out=../  --zrpc_out=../
+#    goctl rpc protoc *.proto --go_out=../ --go-grpc_out=../  --zrpc_out=../ -style=goZero
 #    去除proto中的json的omitempty
 #    mac: sed -i "" 's/,omitempty//g' *.pb.go
 #    linux: sed -i 's/,omitempty//g' *.pb.go
