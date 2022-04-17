90b00d9e16a3defbd43813b6fa7d02cf5b8cb2dd
mikael <13247629622@163.com>
2022-04-17 19:19:26 +0800

    GetUidFromCtx CommonJwtAuthMiddleware

--- common/ctxdata/ctxData.go
+++ common/ctxdata/ctxData.go
@@ -3,12 +3,20 @@ package ctxdata
 import (
 	"context"
 	"encoding/json"
+	"github.com/zeromicro/go-zero/core/logx"
 )
 
 // CtxKeyJwtUserId get uid from ctx
 var CtxKeyJwtUserId = "jwtUserId"
 
 func GetUidFromCtx(ctx context.Context) int64 {
-	uid ,_ := ctx.Value(CtxKeyJwtUserId).(json.Number).Int64()
+	var uid int64
+	if jsonUid, ok := ctx.Value(CtxKeyJwtUserId).(json.Number); ok {
+		if int64Uid, err := jsonUid.Int64(); err == nil {
+			uid = int64Uid
+		} else {
+			logx.WithContext(ctx).Errorf("GetUidFromCtx err : %+v", err)
+		}
+	}
 	return uid
 }
